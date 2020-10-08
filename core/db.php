937da5c57accabb91f2a130f74402e8f364f44bf
Eradah <eradahalfakeh@gmail.com>
2020-10-08 12:12:57 +0300

    query bulider

--- core/db.php
+++ core/db.php
@@ -1,93 +1,197 @@
-<?php
-class DB{
+<?PHP
 
+class DB{
     public $columns="";
     public $tables="";
     public $cond="";
     public $final_query="";
-    private $connection="";
-    public  $stmt="";
-    public $colsHead="";
-    public $cols="";
-    public $DATABASE_HOST = 'localhost';
-    public $DATABASE_USER = 'root';
-    public $DATABASE_PASS = '';
-    public $DATABASE_NAME = 'shop';
-  
-    function __constructor(){
-       
-      
-            $this->connection= new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-       
-            }
-    function select($cols=['*']){
-        $result=$cols=='*' ? $cols : implode(', ',$cols);
-        $this->columns="select ". $result." ";
+    private $connection;
+    public $updateCond="";
+    public $orderBy="";
+    public $count="";
+    public $groupBy="";
+    public $join="";
+
+
+
+
+
+    function __construct($server,$dbname,$username,$pass){
+
+       // $dsn="mysql:host=".$server.";dbname=".$dbname;
+       $dsn="mysql:host=localhost;dbname=shop";
+
+        $this->connection=new PDO($dsn,"root","");
+
+    }
+
+
+    function select($cols){
+        $this->columns="select ".implode(",",$cols)." ";
+
 
         return $this;
 
     }
-
     function from($tbls){
-        $this->tables=" from ".implode(",",$tbls)." ";
+
+        $this->tables="from ".implode(",",$tbls)." ";
+       
         return $this;
-        
+
     }
 
-    function where($cond,$oprator,$value){
+    function orderBy($ordercol,$orderway){
         if(empty($this->cond))
-        $this->cond=" where  ".$cond." ".$oprator." ".$value;
-        else
-        $this->cond.=" and  ".$cond." ".$oprator." ".$value;
-        return $this;
-        
-    }
-    function orwhere($cond,$oprator,$value){
-        if(empty($this->cond))
-        $this->cond=" where  ".$cond." ".$oprator." ".$value;
-        else
-        $this->cond.=" or  ".$cond." ".$oprator." ".$value;
-        return $this;
-        
-    }
-    
-    function bulid(){
-        $this->final_query=$this->columns.$this->tables.$this->cond;
-        return  $this;
+        $this->orderBy="order by ".$ordercol." ".$orderway." ";
+       else
+       $this->orderBy.=",".$ordercol." ".$orderway." ";
+       return $this;
     }
     function count($col){
         $this->count="Count(".$col.") " ;
         return $this;
     }
-    function update(){
-      
-        $this->final_query="UPDATE ".$this->tables." SET  ".$this->columns.$this->cond;
+    function groupBy($ordercol,$orderway){
+        if(empty($this->condation))
+        $this->groupBy="GROUP by ".$ordercol." ".$orderway." ";
+       else
+       $this->groupBy.=",".$ordercol." ".$orderway." ";
+       return $this;
+    }
+    function limit($offset=0,$maxno){
+        $this->limit="limit ".$offset.",".$maxno." " ;
+        return $this;
+    }
+    function innerjoin($table,$leftside,$rightside){
+        $this->join.=" INNER JOIN ".$table." ON ".$leftside." = ".$rightside." ";
+        return $this;
+    }
+    function outerjoin($table,$leftside,$rightside){
+        $this->join.=" RIGHT JOIN ".$table." ON ".$leftside." = ".$rightside." ";
+        return $this;
+    }
+    function leftjoin($table,$leftside,$rightside){
+        $this->join.=" LEFT JOIN ".$table." ON ".$leftside." = ".$rightside." ";
+        return $this;
+    }
+    function where($cond,$oprator,$value){
+        
+
+        if(empty($this->cond))
+
+        $this->cond="where ".$cond." ".$oprator." ".$value." ";
+        else
+        $this->cond.=" and ".$cond." ".$oprator." ".$value." ";
+
         return $this;
 
+
     }
-    
-    function insert($tbla,$cols,$val){
-        $this->colsHead=$cols;
-        $this->final_query= "INSERT INTO ".$tbla." (".$cols.")"." VALUES (".$val.") ";
+    function orWhere($cond,$oprator,$value){
+        if(empty($this->cond))
+
+        $this->cond="where ".$cond." ".$oprator." ".$value." ";
+        else
+        $this->cond.=" or ".$cond." ".$oprator." ".$value." ";
+
         return $this;
     }
-    function delete(){
-         $this->final_query= "DELETE FROM ".$this->tables." ".$this->cond;
-         return $this;
-      
+
+
+    function build(){
+        
+        $this->final_query=$this->columns.$this->tables.$this->cond;
+       
+        return $this;
     }
-    function execute(){
+
+
+    function exeucte(){
+        $stmt=$this->connection->prepare($this->final_query);
+        $stmt->execute();
+       $result= $stmt->fetchAll(PDO::FETCH_OBJ);
+       return $result;
+
+
+    }
+
+
+    function insert($tbl,$items){
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $values=array();
+        foreach(array_values($items)as $item){
+            $values[]="'".$item."'";
+        }
+       try{
+        $this->final_query="insert into ".$tbl."(".implode(",",array_keys($items)).") values (".implode(",",$values).")";
+        
+        $stmt=$this->connection->prepare($this->final_query);
+        $stmt->execute();
         echo $this->final_query;
-        $this->stmt = $this->connection->prepare($this->final_query);
-           $this->stmt->execute();
+       }catch(Exception $ex){
+           
+           echo $ex->getMessage();
+       }
         return $this;
 
     }
-    function fetch(){
-        $result= $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-          return  $this;
-    }
-}
 
+    
+    function delete($tbl){
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      
+       try{
+        $this->final_query="delete from ".$tbl." ".$cond." ";
+        
+        $stmt=$this->connection->prepare($this->final_query);
+        $stmt->execute();
+        echo $this->final_query;
+       }catch(Exception $ex){
+           
+           echo $ex->getMessage();
+       }
+        return $this;
+
+    }
+
+    function updateCond($cond,$oprator,$value){
+        if(empty($this->cond))
+
+        $this->updateCond="SET ".$cond." ".$oprator." ".$value." ";
+        else
+        $this->updateCond.=" , ".$cond." ".$oprator." ".$value." ";
+
+        return $this;
+    }
+    function update($tbl,$items){
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       
+       try{
+        $this->final_query="update  ".$tbl."  " . $this->updateCond." ".$cond." ";
+        
+        $stmt=$this->connection->prepare($this->final_query);
+        $stmt->execute();
+        echo $this->final_query;
+       }catch(Exception $ex){
+           
+           echo $ex->getMessage();
+       }
+        return $this;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 ?>
